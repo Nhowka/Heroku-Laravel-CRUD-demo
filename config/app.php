9871c4aef62dbd20e2f39315d512f25e53590405
Mike Rogers <me@mikerogers.io>
2015-09-16 14:03:10 +0100

    Adding in scaffolding generator
    https://packagist.org/packages/appzcoder/crud-generator - This is the
    package i used.

--- config/app.php
+++ config/app.php
@@ -145,6 +145,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Appzcoder\CrudGenerator\CrudGeneratorServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -193,6 +196,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Form'      => Collective\Html\FormFacade::class, 
+        'HTML'      => Collective\Html\HtmlFacade::class,
 
     ],
 
